94b2e4708e3749fbec003854f58ae946d5f9b6aa
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-11-15 18:36:09 +0530

    Clustering in Action

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
@@ -11,13 +12,21 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-  res.send('Performance example');
+  res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   // delay the response
   delay(9000);
-  res.send('Din ding ding!');
+  res.send(`Din ding ding! ${process.pid}`);
 });
 
-app.listen(3000);
+console.log('Running server.js');
+if (cluster.isMaster) {
+  console.log('Master Has been started...');
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log('Worker process started...');
+  app.listen(3000);
+}
